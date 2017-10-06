183847bc9e9bedf4d4f5e56b6ccda20839890a4f
Johan Martinsson <j.martinsson@bee-buzziness.com>
2017-10-06 22:32:59 +0100

    minor improvement

--- java/src/test/java/com/codurance/training/tasks/MyDateShould.java
+++ java/src/test/java/com/codurance/training/tasks/MyDateShould.java
@@ -7,24 +7,20 @@ import static org.junit.Assert.*;
 
 public class MyDateShould {
 
-    @Test
-    public void isComparableLess() throws Exception {
-        MyDate b4 = new MyDate("01012000");
-        MyDate after = new MyDate("02012000");
-        assertThat(b4, Matchers.lessThan(after));
-    }
+    MyDate b4 = new MyDate("01012000");
+    MyDate after = new MyDate("02012000");
 
     @Test
-    public void isComparableGreater() throws Exception {
-        MyDate b4 = new MyDate("01012000");
-        MyDate after = new MyDate("02012000");
+    public void isComparableLess() throws Exception {
+        assertThat(b4, Matchers.lessThan(after));
         assertThat(after, Matchers.greaterThan(b4));
     }
 
+
     @Test
     public void isComparableEqual() throws Exception {
-        MyDate someDate = new MyDate("01012000");
-        MyDate anEqualDate = new MyDate("01012000");
+        MyDate someDate = new MyDate("25012000");
+        MyDate anEqualDate = new MyDate("25012000");
         assertThat(someDate, Matchers.comparesEqualTo(anEqualDate));
     }
 }
